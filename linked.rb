49a5f26387fd5b961e4af5aa3dfe2cb57ff392df
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-09 07:46:47 -0700

    Set up method names

---
+++ linked.rb
@@ -0,0 +1,47 @@
+class LinkedList
+    def append(value)
+    end
+
+    def prepend(value)
+    end
+
+    def size
+    end
+
+    def head
+    end
+
+    def tail
+    end
+
+    def at(index)
+    end
+
+    def pop
+    end
+
+    def contains?(value)
+    end
+
+    def find(value)
+    end
+
+    def to_s
+    end
+end
+
+
+
+
+
+
+
+class Node
+    def value
+        @value = nil
+    end
+
+    def next_node
+        @next_node = nil
+    end
+end
