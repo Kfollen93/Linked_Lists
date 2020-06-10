d6e67b1e85e68eeb97c77c7388b53548a9479ba3
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-10 10:09:54 -0700

    Refactored methods

--- linked.rb
+++ linked.rb
@@ -1,47 +1,79 @@
+class Node
+attr_accessor :value, :next_node
+
+    def initialize(value = nil, next_node = nil)
+        @value = value
+        @next_node = next_node
+    end
+end
+
 class LinkedList
-    def append(value)
+    attr_accessor :name, :head, :tail, :size
+    def initialize
+        @head = nil
+        @tail = nil
+        @size = 0
     end
 
-    def prepend(value)
+    def append(node)
+        node = Node.new(node)
+        if @head == nil
+            @head = node
+        else
+            @tail.next_node = node
+        end
+            @tail = node
+            @size += 1
+    end
+
+    def prepend(node)
+        node = Node.new(node)
+        if @head == nil
+            @head = node
+        else
+            @head = current_node
+            current_node = @head.next_node
+            @head = node
+            @size += 1
+        end
     end
 
     def size
+        puts "The current list size is: #{@size}"
     end
-
+    
     def head
+        puts "The current list head is: #{@head.value}"
     end
 
     def tail
+        puts "The current list head is: #{@tail.value}"
     end
 
     def at(index)
-    end
-
-    def pop
-    end
-
-    def contains?(value)
-    end
-
-    def find(value)
+        
     end
 
     def to_s
+        node = @head
+        until node.nil?
+            print " ( #{node.value} ) ->"
+            node = node.next_node
+        end
+        print " nil\n"
     end
+
 end
 
+alpha = LinkedList.new
+alpha.append('A')
+alpha.append('B')
+alpha.append('C')
+alpha.append('D')
 
 
+alpha.to_s
+alpha.size
+alpha.head
+alpha.tail
 
-
-
-
-class Node
-    def value
-        @value = nil
-    end
-
-    def next_node
-        @next_node = nil
-    end
-end
